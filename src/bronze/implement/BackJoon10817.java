5a086a83cc3328844b78605efaafdea71f0faf1f
tany <juni8453@naver.com>
2021-12-14 16:18:04 +0900

    백준 10917번 세 수 비교 문제를 풀었습니다.

---
+++ src/bronze/implement/BackJoon10817.java
@@ -0,0 +1,28 @@
+package bronze.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BackJoon10817 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] a = new int[3];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+
+        System.out.println(a[1]);
+    }
+}
+
+// 세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오.
+// 공백으로 세 수를 한 줄에 입력받는다.
