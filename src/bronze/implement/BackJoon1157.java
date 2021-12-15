7763b298aafbaffc5a0c64752241f5365117a32c
tany <juni8453@naver.com>
2021-12-15 23:48:39 +0900

    백준 1197번 단어 공부 문제를 풀었습니다.

---
+++ src/bronze/implement/BackJoon1157.java
@@ -0,0 +1,36 @@
+package bronze.implement;
+
+import java.io.*;
+
+public class BackJoon1157 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] arr = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            if ('a' <= str.charAt(i) && str.charAt(i) <= 'z') {
+                arr[str.charAt(i)-'a']++;
+            }
+
+            else if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
+                arr[str.charAt(i)-'A']++;
+            }
+        }
+
+        int max = 0;
+        char result = ' ';
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                result = (char)(i+'A');
+
+            } else if (arr[i] == max) {
+                result = '?';
+            }
+        }
+
+        System.out.println(result);
+    }
+}
