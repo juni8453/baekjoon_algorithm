98117192d005c6bd3472a5799d6386fc5a176469
tany <juni8453@naver.com>
2021-12-16 15:13:17 +0900

    백준 11721번 열 개씩 끊어 출력하기 문제를 풀었습니다.

---
+++ src/bronze/implement/BaekJoon11721.java
@@ -0,0 +1,21 @@
+package bronze.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon11721 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i));
+            // 9번째 인덱스일 때 개행
+            if (i % 10 == 9) {
+                System.out.println();
+            }
+        }
+    }
+}
