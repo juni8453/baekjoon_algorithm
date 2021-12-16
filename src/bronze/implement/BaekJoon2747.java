b044514c629469b84187821d0ae4db5735c291cb
tany <juni8453@naver.com>
2021-12-16 19:55:05 +0900

    백준 2747번 피보나치 수 문제를 풀었습니다.

---
+++ src/bronze/implement/BaekJoon2747.java
@@ -0,0 +1,23 @@
+package bronze.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon2747 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arrNumbers = new int[n + 1];
+        arrNumbers[0] = 0;
+        arrNumbers[1] = 1;
+
+        for (int i = 2; i < arrNumbers.length; i++) {
+            arrNumbers[i] = arrNumbers[i - 1] + arrNumbers[i - 2];
+        }
+        System.out.println(arrNumbers[n]);
+    }
+}
+
+// n = 17
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
