35386c3c134505d4cf4e3750a2a7baac7150096f
tany <juni8453@naver.com>
2021-12-20 20:47:32 +0900

    프로그래머스 level1 수박수박수박수박수? 문제를 풀었습니다.

--- src/level1/ProgrammersLevel1_10.java
+++ src/level1/ProgrammersLevel1_10.java
@@ -1,2 +1,30 @@
-package level1;public class ProgrammersLevel1_10 {
+package level1;
+
+import java.util.Scanner;
+
+public class ProgrammersLevel1_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String answer = solution (n);
+        System.out.println(answer);
+
+    }
+
+    private static String solution(int n) {
+        String answer = " ";
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                answer += "수";
+            }
+
+            if (i % 2 != 0) {
+                answer += "박";
+            }
+        }
+
+        return answer;
+    }
 }
