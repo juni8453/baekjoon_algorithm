a469b54f56f30cd5018a89ed571ec3703a9c3b1f
tany <juni8453@naver.com>
2021-12-22 16:24:43 +0900

    프로그래머스 level1 문자열 내 마음대로 정렬하기 문제를 풀었습니다.

---
+++ src/level1/ProgrammersLevel1_20.java
@@ -0,0 +1,45 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class ProgrammersLevel1_20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        String[] inputStr = new String[count];
+
+        for (int i = 0; i < inputStr.length; i++) {
+            inputStr[i] = sc.next();
+        }
+
+        int n = sc.nextInt();
+
+        String[] answer = solution(inputStr, n);
+
+        Arrays.stream(answer).forEach(s -> System.out.print(s + " "));
+    }
+
+    private static String[] solution(String[] inputStr, int n) {
+        String[] answer = new String[inputStr.length];
+
+        for (int i = 0; i < inputStr.length; i++) {
+            char checkSecondChar = inputStr[i].charAt(n);
+            answer[i] = checkSecondChar + inputStr[i];
+        }
+
+        Arrays.sort(answer, Comparator.naturalOrder());
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answer[i].substring(1);
+        }
+
+
+        return answer;
+    }
+}
+
+// 인덱스 n의 문자가 같은 문자열이 여럿 일 경우
+// 사전순으로 앞선 문자열이 앞쪽에 위치한다.
+// [abce, abcd, cdx] -> return [abcd, abce, cdx]
