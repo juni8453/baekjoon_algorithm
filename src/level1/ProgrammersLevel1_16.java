c971859f93b44e3dfea51082d2d66a7cd1f0a1a9
tany <juni8453@naver.com>
2021-12-22 13:31:02 +0900

    프로그래머스 level1 서울에서 김서방 찾기 문제를 풀었습니다.

---
+++ src/level1/ProgrammersLevel1_16.java
@@ -0,0 +1,34 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ProgrammersLevel1_16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String[] strings = new String[n];
+
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = sc.next();
+        }
+
+        String answer = solution(strings);
+        System.out.println(answer);
+    }
+
+    private static String solution(String[] strings) {
+        String answer = "";
+        int count = 0;
+
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].equals("Kim")) {
+                count = i;
+                answer = "김서방은" + " " + count + "에 있다";
+            }
+        }
+
+        return answer;
+    }
+}
