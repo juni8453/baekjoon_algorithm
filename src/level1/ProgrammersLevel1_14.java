7ce585f4b931697845b807a6f37bb032f85c4882
tany <juni8453@naver.com>
2021-12-22 10:12:01 +0900

    프로그래머스 level1 자연수 뒤집어 배열로 만들기 문제를 풀었습니다.

---
+++ src/level1/ProgrammersLevel1_14.java
@@ -0,0 +1,31 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ProgrammersLevel1_14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        int[] answer = solution(n);
+        System.out.println(Arrays.toString(answer));
+    }
+
+    public static int[] solution(long n) {
+        String str = String.valueOf(n);
+        int length = str.length();
+        int[] answer = new int[length];
+        int[] reverseAnswer = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            answer[i] = Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+
+        for (int i = 0; i < length; i++) {
+            reverseAnswer[i] = answer[length - i - 1];
+        }
+
+        return reverseAnswer;
+    }
+}
