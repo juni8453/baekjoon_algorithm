b07241f5e86cc35dd0716993c045bcb22edc5de3
tany <juni8453@naver.com>
2021-12-31 17:52:20 +0900

    프로그래머스 level1 음양 더하기 문제를 풀었습니다.

---
+++ src/level1/ProgrammersLevel1_35.java
@@ -0,0 +1,47 @@
+package level1;
+
+import java.util.Scanner;
+
+public class ProgrammersLevel1_35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int aCount = sc.nextInt();
+        int bCount = sc.nextInt();
+
+        int[] absolutes = new int[aCount];
+        boolean[] signs = new boolean[bCount];
+
+        for (int i = 0; i < absolutes.length; i++) {
+            absolutes[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < signs.length; i++) {
+            signs[i] = sc.nextBoolean();
+        }
+
+        int answer = solution(absolutes, signs);
+        System.out.println(answer);
+
+    }
+
+    private static int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                absolutes[i] = Math.abs(absolutes[i]);
+            }
+
+            else {
+                absolutes[i] *= -1;
+            }
+
+            answer += absolutes[i];
+        }
+
+        return answer;
+    }
+}
+
+
+// (n * -1) 양수는 음수로, 음수는 양수로 전환
