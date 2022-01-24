8238bf342c6ac779e7df533c6d6d22faeedc9acf
tany <juni8453@naver.com>
2022-01-24 10:19:07 +0900

    38번 주석 추가.

--- src/level1/ProgrammersLevel1_38.java
+++ src/level1/ProgrammersLevel1_38.java
@@ -1,7 +1,6 @@
 package level1;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -29,20 +28,29 @@ public class ProgrammersLevel1_38 {
 
         // Todo 두 배열을 비교해서 participant 배열에 있지만 completion 배열에 없는 값을 찾아낸다.
         //  동명이인일 경우도 존재하니까 List 말고 Key 값으로 빠르게 찾을 수 있는 Map 을 사용해보자.
+        //  동명이인은 Key의 중복이니까 찾고자 하는 키가 있으므로 1을 반환 후 +1 하면 2가 된다.
 
         Map<String, Integer> map = new HashMap<>();
 
         for (String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
+            // getOrDefault(Object key, V DefaultValue)
+            // 찾는 키가 존재한다면 찾는 키의 값을 반환하고 없다면 기본 값을 반환한다.
+            // key : 값을 가져와야 하는 요소의 키
+
+            // 즉, name 이라는 키, name 이라는 키 값이 존재하면 name 을 넣어주고 없다면 0을 넣어줌, 그리고 + 1
+            // eden 키가 없으면 eden 에 1이 들어가고 있다면 eden 값을 반환하며 거기에다가 + 1
+            map.put(name, map.getOrDefault(name,0) + 1);
         }
 
         for (String name : completion) {
+            // 지정된 키의 값에 -1 하고 다시 map.put
             map.put(name, map.get(name) - 1);
         }
 
-        for (String key : map.keySet()) {
-            if (map.get(key) > 0) {
-                answer = key;
+        for (String name : map.keySet()) {
+            // name key의 값이 0보다 크다면 ? (완주한 경우)
+            if (map.get(name) > 0) {
+                answer = name;
                 break;
             }
         }
