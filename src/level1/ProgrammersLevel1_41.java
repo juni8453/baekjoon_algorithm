3705ce5bc4efb084d02a6d1d0a79dbd759b613f3
tany <juni8453@naver.com>
2022-02-03 20:28:47 +0900

    프로그래머스 level1 행렬의 덧셈 문제를 풀었습니다.

---
+++ src/level1/ProgrammersLevel1_41.java
@@ -0,0 +1,54 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ProgrammersLevel1_41 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int column = sc.nextInt();
+        int row = sc.nextInt();
+
+        int[][] arr1 = new int[column][row];
+        int[][] arr2 = new int[column][row];
+
+        for (int i = 0; i < column; i++) {
+            for (int k = 0; k < row; k++) {
+                arr1[i][k] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < column; i++) {
+            for (int k = 0; k < row; k++) {
+                arr2[i][k] = sc.nextInt();
+            }
+        }
+
+        int[][] result = ProgrammersLevel1_41.solution(arr1, arr2);
+
+        Arrays.stream(result).forEach(arr -> System.out.print(Arrays.toString(arr) + " "));
+    }
+
+    private static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = arr1;
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int k = 0; k < answer[i].length; k++) {
+                answer[i][k] = arr1[i][k] + arr2[i][k];
+            }
+        }
+
+        return answer;
+
+    }
+}
+
+
+// 2개의 행렬 arr1, arr2 를 입력받아, 행렬 덧셈의 결과를 반환하는 함수를 구현해라.
+// arr1   = { {1,2}, {2,3} }
+// arr2   = { {3,4}, {5,6} }
+// answer = { {4,6}, {7,9} }
+
+// arr1[0][0] 1 + arr2[0][0] 3 = 4 -> answer[0][0] = 4
+// arr1[0][1] 2 + arr2[0][1] 4 = 6 -> answer[0][1] = 6
