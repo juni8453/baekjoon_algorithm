39c9a3b1c3bb8d1aac3ceb7a3497af624c10e279
ByeoungJoon Jeon <juni8453@naver.com>
2022-04-06 08:14:42 +0900

    대소문자를 구분하지 않는 문자 찾기

---
+++ src/study/Section0101.java
@@ -0,0 +1,29 @@
+package study;
+
+import java.util.Scanner;
+
+public class Section0101 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+        int count = 0;
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        String[] split = a.split("");
+
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].contains(b)) {
+                count += 1;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
+
+// 한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개가 존재하는지 알아내는 프로그램을 작성해라.
+// 대,소문자를 구분하지 않고 문자열의 길이는 100을 넘지 않는다.
---
+++ src/study/Section0101Answer.java
@@ -0,0 +1,38 @@
+package study;
+
+import java.util.Scanner;
+
+public class Section0101Answer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        char b = sc.next().charAt(0);
+
+        int answer = Section0101Answer.solution(a, b);
+        System.out.println(answer);
+    }
+
+    private static int solution(String a, char b) {
+        int answer = 0;
+
+        a = a.toUpperCase();
+        b = Character.toUpperCase(b);
+
+//        for (int i = 0; i < a.length(); i++) {
+//            if (a.charAt(i) == b) {
+//                answer++;
+//            }
+//        }
+
+        // 향상된 for 문으로 풀기 (뒤에는 배열,컬렉션이 올 수 있으므로 배열로 바꾸고 진행)
+        // a.toCharArray() == a.split("")
+        for (char x : a.toCharArray()) {
+            if (x == b) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
