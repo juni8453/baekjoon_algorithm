5526cbd6ea4fb4c15c3dd3c239a40db3c4c83176
ByeoungJoon Jeon <juni8453@naver.com>
2022-09-23 21:57:11 +0900

    프로그래머스 DFS/BFS 여행경로 문제풀이 시작

---
+++ src/programmers/bfs/여행경로.java
@@ -0,0 +1,25 @@
+package programmers.bfs;
+
+import java.util.Arrays;
+
+public class 여행경로 {
+    public static void main(String[] args) {
+        String[][] tickets = {
+            {"ICN", "JFK"},
+            {"HND", "IAD"},
+            {"JFK", "HND"}
+        };
+
+        System.out.println(Arrays.toString(new Solution1().solution(tickets)));
+    }
+}
+
+class Solution1 {
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+
+
+
+        return answer;
+    }
+}
