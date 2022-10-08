7347350a227b582bdd910d14f855eb7e20b27a06
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-08 22:39:48 +0900

    구름 알고리즘먼데이 챌린지 시작

--- src/groom/week_1/동명이인.java
+++ src/groom/week_1/동명이인.java
@@ -1,2 +1,9 @@
-package groom.week_1;public class 동명이인 {
+package groom.week_1;
+
+import java.io.BufferedReader;
+
+public class 동명이인 {
+    public static void main(String[] args) {
+        
+    }
 }
--- src/groom/week_1/소수찾기.java
+++ src/groom/week_1/소수찾기.java
@@ -1,2 +1,42 @@
-package groom.week_1;public class 소수찾기 {
+package groom.week_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 소수찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (isPrime(arr[i])) {
+                sum += arr[i];
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    private static boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
