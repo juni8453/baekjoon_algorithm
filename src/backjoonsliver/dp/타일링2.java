8a79e30b952578ccabeed67d612abe53d9686b64
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-16 23:33:05 +0900

    백준 DP 타일링2 문제풀이

---
+++ src/backjoonsliver/dp/타일링2.java
@@ -0,0 +1,25 @@
+package backjoonsliver.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 타일링2 {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[1001];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
