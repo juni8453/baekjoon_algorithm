75dc647c98b0566829db6f4470cc4399cfa1bbaa
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-17 13:52:34 +0900

    구름 챌린지 출석부 문제풀이:

--- src/groom/week_2/출석부.java
+++ src/groom/week_2/출석부.java
@@ -7,9 +7,9 @@ import java.util.*;
 class Person implements Comparable<Person> {
 
     String name;
-    double high;
+    String high;
 
-    public Person(String name, double high) {
+    public Person(String name, String high) {
         this.name = name;
         this.high = high;
     }
@@ -19,14 +19,14 @@ class Person implements Comparable<Person> {
     @Override
     public int compareTo(Person o) {
         if (this.name.equals(o.name)) {
-            return (int) (this.high - o.high);
+            return this.high.compareTo(o.high);
         }
 
         return this.name.compareTo(o.name);
     }
 }
 
-public class 출석부2 {
+public class 출석부 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // 사람의 수
@@ -35,15 +35,13 @@ public class 출석부2 {
 
         for (int i = 0; i < n; i++) {
             String name = sc.next();
-            double high = sc.nextDouble();
+            String high = sc.next();
 
             personInfo.add(new Person(name, high));
         }
 
         Collections.sort(personInfo);
 
-
-
-        System.out.print(personInfo.get(k - 1).name + " " + personInfo.get(k - 1).high + 0);
+        System.out.print(personInfo.get(k - 1).name + " " + personInfo.get(k - 1).high);
     }
 }
