0b84e8c92af150017b8f79085ebe1517eaef7f1d
ByeoungJoon Jeon <juni8453@naver.com>
2022-11-15 20:32:32 +0900

    위상정렬 볷,ㅂ

--- src/baekjoongold/선수과목.java
+++ src/baekjoongold/선수과목.java
@@ -8,77 +8,67 @@ import java.util.*;
 public class 선수과목 {
 
     static int[] result;
+    static int[] indegree;
+    static List<List<Integer>> graph = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 과목의 수
+        int m = Integer.parseInt(st.nextToken()); // 선수 조건의 수
+        indegree = new int[n + 1];
+        result = new int[n + 1];
 
-        // 과목의 수 N (1 <= N <= 1,000)
-        // 선수 조건의 수 M (0 <= M <= 500,000)
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        List<List<Integer>> array = new ArrayList<>(); // 2차원 인접리스트
-        int[] indegree = new int[n + 1]; // 해당 노드를 가리키는 진입 차수를 저장하는 배열
-        result = new int[n + 1]; // 갱신을 위한 배열
-
-        // 2차원 인접리스트 생성을 위해 초기화
+        // 단방향 2차원 리스트 초기화
         for (int i = 0; i <= n; i++) {
-            array.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
 
-        // 2차원 인접리스트 생성 및 진입 차수 저장 배열 셋팅
+        // 단방향 2차원 리스트 설정
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int aSubject = Integer.parseInt(st.nextToken());
+            int bSubject = Integer.parseInt(st.nextToken());
 
-            array.get(a).add(b);
-            indegree[b]++;
+            graph.get(aSubject).add(bSubject);
+            indegree[bSubject]++; // 각 인덱스 번호 = 노드 번호이므로 진입차수를 하나씩 카운트
         }
 
-        topologicalSort(indegree, array);
-
-        for (int i = 1; i <= n; i++) {
-            System.out.print(result[i] + " ");
-        }
+        topologicalSort();
     }
 
-    private static void topologicalSort(int[] indegree, List<List<Integer>> array) {
-        Queue<Integer> queue = new LinkedList<>(); // 진입 차수가 0이 된 노드들을 담기 위한 큐
+    private static void topologicalSort() {
+        Queue<Integer> queue = new LinkedList<>();
 
-        // queue 에 진입차수가 0 인 노드를 담는다.
+        // 1. 진입차수가 0인 노드 큐에 삽입
         for (int i = 1; i < indegree.length; i++) {
             if (indegree[i] == 0) {
                 queue.offer(i);
-                result[i] = 1; // 진입차수가 0이라면 한 학기에 들을 수 있는 과목이므로 1로 셋팅
+                result[i] = 1; // 진입차수 0 이라면 한 학기에 들을 수 있기 때문에 1로 셋팅
             }
         }
 
+        // 큐가 빌 때 까지 아래와 같은 작업 반복
+        // 1. poll() 이후 해당 노드에서 출발하는 간선 제거
+        // 2. 새롭게 진입차수가 0 이 된 노드 큐에 삽입
         while (!queue.isEmpty()) {
-            int node = queue.poll();
+            int nowNode = queue.poll();
 
-            for (int nextNode : array.get(node)) {
-                indegree[nextNode]--;
+            for (int nextNode : graph.get(nowNode)) {
+                indegree[nextNode]--; // 간선 제거
 
-                if (indegree[nextNode] == 0) {
+                if (indegree[nextNode] == 0) { // 확인 후 진입차수가 0 이라면 큐에 삽입
                     queue.offer(nextNode);
-                    result[nextNode] = result[node] + 1; // 이전 과목을 완료하기 위한 학기 수에서 +1 한 값으로 셋팅
+                    result[nextNode] = result[nowNode] + 1;
                 }
             }
+        }
+
+        for (int i = 1; i < result.length; i++) {
+            System.out.print(result[i] + " ");
         }
     }
 }
 
-/**
- 1. 한 학기에 들을 수 있는 과목 수에는 제한이 없다.
- 2. 모든 과목은 매 학기 항상 개설된다.
- 1번 과목부터 N 번 과목까지 차례대로 최소 몇 학기에 이수할 수 있는지를 한 줄에 공백으로 구분해 출력한다.
- 위상 정렬의 경우 진행 중 진입 차수가 0인 정점이 없다면 사용할 수 없는데, 선수 과목 조건 중 A < B 라는 조건이 있기에 위상정렬을 사용할 수 있다.
- */
-
-/**
- * a b
- * 2 3 -> 2번 과목이 3번 과목의 선수과목
- * 1 2 -> 1번 과목이 2번 과목의 선수과목
- */
+// 순서가 정해져있는 작업을 차례로 수행해야할 때 그 순서를 결정하기 위해 사용하는 알고리즘
+// 사이클이 발생하지 않는 방향 그래프일 떄 사용할 수 있다.
