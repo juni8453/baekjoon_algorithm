b928aa40b2fca1a67d91bd93d6a5d25ee9b07e1c
ByeoungJoon Jeon <juni8453@naver.com>
2022-11-23 20:12:19 +0900

    프로그래멋 스택/큐 프린터 네이밍 변경

--- src/programmers/stack_queue/프린터.java
+++ src/programmers/stack_queue/프린터.java
@@ -1,50 +1,74 @@
 package programmers.stack_queue;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class 프린터 {
-    public static void main(String[] args) {
-        int[] priorities2 = {1, 1, 9, 1, 1, 1};
-        int location2 = 0;
+class Pair {
+    int index;
+    int value;
 
-        System.out.println(new Solution2().solution(priorities2, location2));
+    public Pair(int index, int value) {
+        this.index = index;
+        this.value = value;
     }
 }
 
-class Solution2 {
-    // priorities[] = 중요도 순서 배열 변수
-    // location = 내가 인쇄를 요청한 문서가 어떤 위치에 있는지 알려주는 변수
-    public int solution(int[] priorities, int location) {
-        int answer = 0;
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder()); // 우선순위 큐 정수 내림차순 정렬
+public class 프린터 {
+    public static void main(String[] args) {
+        int[] priorities = {2, 1, 3, 2};
+        int location = 2;
 
+        int[] priorities2 = {1, 1, 9, 1, 1, 1};
+        int location2 = 0;
+
+        int[] priorities3 = {2, 1, 2, 1, 2, 1, 2, 1, 2};
+        int location3 = 1;
+
+        System.out.println(new Solution2_1().solution(priorities, location));
+        System.out.println(new Solution2_1().solution(priorities2, location2));
+        System.out.println(new Solution2_1().solution(priorities3, location3));
+    }
+}
+
+class Solution2_1 {
+    public int solution(int[] priorities, int location) {
+        Queue<Pair> queue = new LinkedList<>();
+        int answer = 0;
+
+        // Queue 셋팅
         for (int i = 0; i < priorities.length; i++) {
-            priorityQueue.offer(priorities[i]);
+            queue.offer(new Pair(i, priorities[i]));
         }
 
-        // 모든 인쇄 대기 목록이 빌 때 까지 반복
-        while (!priorityQueue.isEmpty()) {
-            for (int i = 0; i < priorities.length; i++) { // 큐 값과 매칭되는 경우를 탐색
-               if (priorities[i] == priorityQueue.peek()) { // 값만 일치하는 경우 인쇄
-                   priorityQueue.poll();
-                   answer++;
+        // Queue 에서 가장 앞에 위치한 Pair 를 뽑아낸 뒤, Queue 에 남아있는 Pair 를 확인해 방금 뽑은 Pair 보다
+        // 더 높은 우선순위를 가진 작업이 존재하는지 확인한다.
+        int nowIndex = 0;
+        while (!queue.isEmpty()) {
+            Pair currentPair = queue.poll();
+            boolean flag = false;
 
-                   if (i == location) { // 값과 위치가 모두 일치하면 answer 반
-                       return answer;
-                   }
+            for (Pair pair : queue) {
+                if (pair.value > currentPair.value) {
+                    flag = true; // 현재 뽑아낸 Pair 보다 더 큰 우선순위를 가진 Pair 가 있는 경우
+                }
+            }
+
+            // 현재 뽑아낸 Pair 보다 더 큰 우선순위를 가진 Pair 가 있다면 현재 Pair 를 Queue 뒤로 보내야 한다.
+            if (flag) {
+                queue.offer(currentPair);
+
+            // 더 큰 우선순위를 가진 Pair 가 없다면
+            } else {
+                nowIndex++;
+
+                if (currentPair.index == location) { // 현재 Pair 의 인덱스와 찾으려는 location 이 같다면
+                    answer = nowIndex;
+                    break;
                 }
             }
         }
 
+
         return answer;
     }
 }
-
-/**
- * 1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
- * 2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
- * 3. 그렇지 않으면 J를 인쇄합니다.
- */
-
-// 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 return 하는 프로그램을 작성해라.
