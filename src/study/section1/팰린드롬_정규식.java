8770cae82f6e01d1153a6ff8d76bfe38ec45fe0c
ByeoungJoon Jeon <juni8453@naver.com>
2022-11-27 15:43:54 +0900

    문자열 문제 복습

---
+++ src/study/section1/팰린드롬_정규식.java
@@ -0,0 +1,14 @@
+package study.section1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 팰린드롬_정규식 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+
+    }
+}
