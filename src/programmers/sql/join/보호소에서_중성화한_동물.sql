ca43d6b0fc47612be76ecb2031102baba6cc10ed
ByeoungJoon Jeon <juni8453@naver.com>
2022-12-10 20:26:49 +0900

    프로그래머스 SQL 중성화한 동물 복습

--- src/programmers/sql/join/보호소에서_중성화한_동물.sql
+++ src/programmers/sql/join/보호소에서_중성화한_동물.sql
@@ -4,7 +4,6 @@ FROM ANIMAL_INS ins
 WHERE ins.SEX_UPON_INTAKE != outs.SEX_UPON_OUTCOME
 ORDER BY ins.ANIMAL_ID ASC
 
-# 보호소에 들어올 당시 중성화 x, 입양 시 중성화 된 동물 목록 조회
 # 아이디 순으로 조회
 # 중성화 x = Intact
 # 중성화 o = Spayed or Neutered
